e021635b1bd67a4a47d44612002b03568fbd04f4
Henry <hhen5562@gmail.com>
2024-08-28 21:33:56 +0000

    frontend > DONE

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Login = () => {
   return (
     <form className="login">
+      <h1>Login</h1>
       <input type="text" placeholder="username" />
       <input type="password" placeholder="password" />
       <button className="button-45">Login</button>
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Register = () => {
   return (
     <form className="register">
+      <h1>Register</h1>
     <input type="text" placeholder="username" />
     <input type="password" placeholder="password" />
     <button className="button-45">Register</button>
