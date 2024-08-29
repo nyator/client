a0f06597b1ae301d169c01d8a3ecb3bcf10dcc4d
Henry <hhen5562@gmail.com>
2024-08-29 01:46:43 +0000

    "Updated API to use CORS, changed API endpoint from GET / to POST /register, and updated package dependencies."

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Register = () => {
-  return (
-    <form className="register">
-      <h1>Register</h1>
-    <input type="text" placeholder="username" />
-    <input type="password" placeholder="password" />
-    <button className="button-45">Register</button>
-  </form>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");  
 
-export default Register
+  async function register(ev){
+    ev.preventDefault()
+    fetch('http://localhost:3000',{
+      method: 'POST',
+      body: JSON.stringify({username,password}),
+      headers:{'Content-Type' : 'application/json'},
+    } )
+  }
+
+  return (
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={ev => setUsername(ev.target.value)}/>
+
+      <input 
+        type="password" 
+        placeholder="password" 
+        value={password} 
+        onChange={ev => setPassword(ev.target.value)}/>
+
+      <button className="button-45">Register</button>
+    </form>
+  );
+};
+
+export default Register;
