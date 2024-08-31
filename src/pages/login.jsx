f74273036eab59d6593eaa7bfa867b691bf2335a
Henry <hhen5562@gmail.com>
2024-08-31 00:55:56 +0000

    working on the login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function login(ev) {
+    ev.preventDefault();
+    await fetch("http://localhost:3000/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+      });
+  }
+
   return (
-    <form className="login">
+    <form className="login" onSubmit={login}>
       <h1>Login</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
       <button className="button-45">Login</button>
     </form>
   );
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -2,21 +2,21 @@ import React, { useState } from "react";
 
 const Register = () => {
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");  
+  const [password, setPassword] = useState("");
 
-  async function register(ev){
-    ev.preventDefault()
-    const response =  await fetch('http://localhost:3000/register',{
-        method: 'POST',
-        body: JSON.stringify({username,password}),
-        headers:{'Content-Type' : 'application/json'},
-      } )
-      console.log(response)
-      if (response.status === 200 ){
-        alert('Registration successful.')
-      }else{
-        alert('Registration failed.')
-      }
+  async function register(ev) {
+    ev.preventDefault();
+    const response = await fetch("http://localhost:3000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log(response);
+    if (response.status === 200) {
+      alert("Registration successful.");
+    } else {
+      alert("Registration failed.");
+    }
   }
 
   return (
@@ -26,13 +26,15 @@ const Register = () => {
         type="text"
         placeholder="username"
         value={username}
-        onChange={ev => setUsername(ev.target.value)}/>
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
 
-      <input 
-        type="password" 
-        placeholder="password" 
-        value={password} 
-        onChange={ev => setPassword(ev.target.value)}/>
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
 
       <button className="button-45">Register</button>
     </form>
