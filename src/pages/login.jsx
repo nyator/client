594af96d84ee910647586a379f486d1d2e01c767
Henry <hhen5562@gmail.com>
2024-08-31 01:30:03 +0000

    Auth SUCCESSFUL

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
+import {Navigate} from 'react-router-dom'
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false)
 
   async function login(ev) {
     ev.preventDefault();
-    await fetch("http://localhost:3000/login", {
+   const response = await fetch("http://localhost:3000/login", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
+      credentials: 'include',
       });
+      if (response.ok){
+          setRedirect(true);
+      }else {
+        alert("Invalid username or password");
+      }
   }
 
+  if (redirect){
+    return <Navigate to={'/'} />
+  }
   return (
     <form className="login" onSubmit={login}>
       <h1>Login</h1>
